10ec055d3a3c7abeaf77c03d7469fc126bf53fe7
Paul Adams <PAdams@ethosgroup.com>
2019-06-19 12:19:35 -0500

    module 13

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,41 +1,19 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { ProductListComponent } from "./products/product-list.component";
-import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
-import { StarComponent } from "./shared/star.component";
 import { WelcomeComponent } from "./home/welcome.component";
-import { HttpClientModule } from "@angular/common/http";
-import { ProductDetailComponent } from "./products/product-detail.component";
-import { ProductDetailGuard } from "./products/product-detail.guard";
+import { ProductModule } from "./products/product.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    WelcomeComponent,
-    ProductDetailComponent
-  ],
+  declarations: [AppComponent, WelcomeComponent],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: "products", component: ProductListComponent },
-      {
-        path: "products/:id",
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
-      { path: "welcome", component: WelcomeComponent },
-      { path: "", redirectTo: "welcome", pathMatch: "full" },
-      { path: "**", redirectTo: "welcome", pathMatch: "full" }
-    ])
+    ProductModule,
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
